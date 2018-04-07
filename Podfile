87d3e0e9729468c88f08584e7485b9ffc74b5de1
Alex Chan <alex.chan@berkeley.edu>
2018-04-07 02:34:20 -0700

    Create views

---
+++ Podfile
@@ -0,0 +1,5 @@
+use_frameworks!
+
+target 'YourApp' do
+    pod 'Socket.IO-Client-Swift', '~> 13.1.0'
+end
---
+++ securemeapp/DeviceCell.swift
@@ -0,0 +1,15 @@
+//
+//  DeviceCell.swift
+//  securemeapp
+//
+//  Created by Alex Chan on 4/2/18.
+//  Copyright © 2018 secure.me. All rights reserved.
+//
+
+import UIKit
+import Socket
+
+class DeviceCell: UICollectionViewCell {
+    @IBOutlet weak var deviceLabel: UILabel!
+    
+}
